39216325b7ab40181f418ed268bff9f34490db51
Peter Vinarčík <44924998+bixorko@users.noreply.github.com>
2020-11-01 22:27:27 +0100

    finished graph ploting
    need to implement warning message for invalid region and argument parsing (if not called from __main__)

--- get_stat.py
+++ get_stat.py
@@ -2,6 +2,15 @@ from download import DataDownloader
 from matplotlib import pyplot as plt
 import os
 
+def add_order_to_axs(axs, rects):
+    i = 1
+    for rect in rects:
+        height = rect.get_height()
+        axs.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                i,#'%d' % int(height),
+                ha='center', va='bottom')
+        i += 1
+
 def plot_stat(data_source, fig_location = None, show_figure = False):
     data_source = data_source[1]
     data2016 = {}
@@ -41,14 +50,23 @@ def plot_stat(data_source, fig_location = None, show_figure = False):
             else:
                 data2020[data_source[64][i]] = 1
 
+    data2016 = {k: v for k, v in sorted(data2016.items(), key=lambda item: item[1], reverse=True)}
     keys1 = data2016.keys()
     values1 = data2016.values()
+
+    data2017 = {k: v for k, v in sorted(data2017.items(), key=lambda item: item[1], reverse=True)}
     keys2 = data2017.keys()
     values2 = data2017.values()
+
+    data2018 = {k: v for k, v in sorted(data2018.items(), key=lambda item: item[1], reverse=True)}
     keys3 = data2018.keys()
     values3 = data2018.values()
+
+    data2019 = {k: v for k, v in sorted(data2019.items(), key=lambda item: item[1], reverse=True)}
     keys4 = data2019.keys()
     values4 = data2019.values()
+
+    data2020 = {k: v for k, v in sorted(data2020.items(), key=lambda item: item[1], reverse=True)}
     keys5 = data2020.keys()
     values5 = data2020.values()
 
@@ -57,16 +75,27 @@ def plot_stat(data_source, fig_location = None, show_figure = False):
     plt.setp(axs, yticks=[0, 10000, 20000], ylabel='Pocet nehod')
     fig.tight_layout()
     fig.set_size_inches(6.5, 9.5)
-    axs[0].bar(keys1, values1)
+
+    rects = axs[0].bar(keys1, values1)
     axs[0].set_title('2016')
-    axs[1].bar(keys2, values2)
+    add_order_to_axs(axs[0], rects)
+
+    rects = axs[1].bar(keys2, values2)
     axs[1].set_title('2017')
-    axs[2].bar(keys3, values3)
+    add_order_to_axs(axs[1], rects)
+
+    rects = axs[2].bar(keys3, values3)
     axs[2].set_title('2018')
-    axs[3].bar(keys4, values4)
+    add_order_to_axs(axs[2], rects)
+
+    rects = axs[3].bar(keys4, values4)
     axs[3].set_title('2019')
-    axs[4].bar(keys5, values5)
+    add_order_to_axs(axs[3], rects)
+
+    rects = axs[4].bar(keys5, values5)
     axs[4].set_title('2020')
+    add_order_to_axs(axs[4], rects)
+
     if fig_location:
         if not os.path.exists(fig_location):
             os.makedirs(fig_location)
@@ -76,3 +105,5 @@ def plot_stat(data_source, fig_location = None, show_figure = False):
 
 data_source = DataDownloader().get_list()
 plot_stat(data_source, fig_location='graphs', show_figure=True)
+
+
